b961858a177c1aa2688af71aee60c6a8bfe6614e
David West <davidpwest1220@gmail.com>
2025-03-17 13:56:15 -0500

    Added firePottery() to modify pottery objects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 // Imports go first
 import { makePottery } from './PotteryWheel.js'
+import { firePottery } from './kiln.js'
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery("mug", 1, 3,)
-let vase = makePottery("vase", 7, 12,)
-let bowl = makePottery("bowl", 5, 6,)
-let plate = makePottery("plate", 2, 8,)
+let mug = makePottery("mug", 1, 3)
+let vase = makePottery("vase", 7, 12)
+let bowl = makePottery("bowl", 5, 6)
+let plate = makePottery("plate", 2, 8)
 let pot = makePottery("pot", 8, 10)
 
 console.log(mug)
@@ -14,6 +15,18 @@ console.log(vase)
 console.log(bowl)
 console.log(plate)
 console.log(pot)
+
+let firedMug = firePottery(mug, 2000)
+let firedVase = firePottery(vase, 2100)
+let firedBowl = firePottery(bowl, 2200)
+let firedPlate = firePottery(plate, 2300)
+let firedPot = firePottery(pot, 2400)
+
+console.log(firedMug)
+console.log(firedVase)
+console.log(firedBowl)
+console.log(firedPlate)
+console.log(firedPot)
 
 // Fire each piece of pottery in the kiln
 
